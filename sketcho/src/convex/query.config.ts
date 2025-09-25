dc2bc2c676f1b05722427476d60a96a20ef8ad86
ojas wane <ojaswane79@gmail.com>
2025-09-26 00:35:54 +0530

    Schema updated

--- sketcho/src/convex/query.config.ts
+++ sketcho/src/convex/query.config.ts
@@ -1,3 +1,15 @@
+import { preloadQuery } from "convex/nextjs";
+import { api } from "../../convex/_generated/api";
+import { convexAuthNextjsToken } from "@convex-dev/auth/nextjs/server";
+
+export const ProfileQuery = async () => {
+    return preloadQuery(
+        api.user.getCurrentUser, 
+        {}, 
+        {token : await convexAuthNextjsToken() }
+    )
+}
+
 // this is nothing but it takes the data from the data base which checks if the user has subscription or not
 export const subscriptionEntitlementQuery =  async () => {
 
