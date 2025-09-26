62d064ed54f0a4eef240bf62d2401f44df422ec2
ojas wane <ojaswane79@gmail.com>
2025-09-26 12:24:23 +0530

    App updated

--- sketcho/src/convex/query.config.ts
+++ sketcho/src/convex/query.config.ts
@@ -1,7 +1,7 @@
 import { preloadQuery, fetchQuery } from "convex/nextjs";
 import { api } from "../../convex/_generated/api";
 import { convexAuthNextjsToken } from "@convex-dev/auth/nextjs/server";
-import { normalizeProfile } from "@/type/user";
+import { ConvexUserRaw, normalizeProfile } from "@/type/user";
 
 export const ProfileQuery = async () => {
     return fetchQuery(
@@ -14,5 +14,6 @@ export const ProfileQuery = async () => {
 // this is nothing but it takes the data from the data base which checks if the user has subscription or not
 export const subscriptionEntitlementQuery =  async () => {
     const raw_profileData = await ProfileQuery();
-    const profile = normalizeProfile(raw_profileData);
+    const profile = normalizeProfile(raw_profileData as unknown as ConvexUserRaw | null);
+    
 }
