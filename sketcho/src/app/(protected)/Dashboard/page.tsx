bdbafe735c42ea280cc0869940064c96f8d1a686
ojas wane <ojaswane79@gmail.com>
2025-09-27 20:19:38 +0530

    App updated

--- sketcho/src/app/(protected)/Dashboard/page.tsx
+++ sketcho/src/app/(protected)/Dashboard/page.tsx
@@ -1,11 +1,13 @@
-import React from 'react'
 import { subscriptionEntitlementQuery } from '@/convex/query.config'
+import { redirect } from 'next/navigation'
+import { combinedSlug } from '@/lib/utils'
 
 const page = async () => {
     const [entitlemnt , profileName ] = await subscriptionEntitlementQuery()
-  return (
-    <div>page</div>
-  )
+    if(!entitlemnt._valueJSON){
+      redirect(`/billing${combinedSlug(profileName!)}`);
+    }
+    redirect(`/dashboard/${combinedSlug(profileName!)}`)
 }
 
 export default page
